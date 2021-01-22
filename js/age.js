4ffce893edb858f373ed92dcb87230ebd28d4fce
Mr. A <ag.perch@gmail.com>
2021-01-21 17:51:56 -0800

    new

--- js/age.js
+++
@@ -1,9 +0,0 @@
-function PopUp(hideOrshow) {
-    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
-    else document.getElementById('ac-wrapper').removeAttribute('style');
-}
-window.onload = function () {
-    setTimeout(function () {
-        PopUp('show');
-    }, 0000);
-}
